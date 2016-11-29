2b615f82bc74921b5ed49ea7715529a03fd744a1
lb7 <luke.baker7@gmail.com>
2016-11-29 15:15:59 -0800

    Added basic routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export class Recipe extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export class HomePage extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export class Search extends React.Component {
+    render() {
+        return (
+            <div>Search</div>
+        )
+    }
+}
+
+export class Results extends React.Component {
+    render() {
+        return (
+            <div>Results</div>
+        )
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router'
+import App, {HomePage, Recipe, Search, Results} from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={HomePage} />
+            <Route path="recipe/:id" component={Recipe} />
+            <Route path="search" component={Search} />
+            <Route path="search/results" component={Results}/>
+        </Route>
+    </Router>,
+    document.getElementById('root')
 );
