5a8cc305c1ff72fdaff8de35ee5ccf1515858786
Lucas Baker <luke.baker7@gmail.com>
2016-11-30 08:50:03 -0800

    Fixed duplicate NavigationLinks

--- src/App.js
+++ src/App.js
@@ -26,21 +26,4 @@ class NavigationLinks extends React.Component {
         );
     }
 }
-
-//navigation mock-up will fix later
-class NavigationLinks extends React.Component{
-    render(){
-        return(
-         <nav>
-            <h2>Navigation</h2>
-            <ul className="list-unstyled">
-                <li><Link to="/" activeClassName="activeLink">Homepage</Link></li>
-                <li><Link to="recipe/:id" activeClassName="activeLink">Recipes</Link></li>
-                <li><Link to="search/results" activeClassName="activeLink">Search Results</Link></li>
-            </ul>
-         </nav>
-
-        );
-    }
-}
 export default App;
