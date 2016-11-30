23bd92b3641e5065bcc851a83d788b50386eccb6
Kar Yin Ng <karyin3@uw.edu>
2016-11-30 01:19:51 -0800

    nav links in App.js - work in progress

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ class App extends Component {
             <div>
                 {this.props.children}
             </div>
+        )
+    }
+}
+
+//navigation mock-up will fix later
+class NavigationLinks extends React.Component {
+    render() {
+        return (
+            <nav>
+                <h2>Navigation</h2>
+                <ul className="list-unstyled">
+                    <li><Link to="/" activeClassName="activeLink">Homepage</Link></li>
+                    <li><Link to="recipe/:id" activeClassName="activeLink">Recipes</Link></li>
+                    <li><Link to="search/results" activeClassName="activeLink">Search Results</Link></li>
+                </ul>
+            </nav>
+
         );
     }
 }
