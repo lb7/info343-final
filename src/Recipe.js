bcd2a5fb2c0b1663a9411ca8ffa9ec058d7a3d7f
kairihye <hyei@uw.edu>
2016-12-06 17:37:13 -0800

    test pulled code

--- src/Recipe.js
+++ src/Recipe.js
@@ -105,7 +105,7 @@ class IngredientList extends React.Component {
         });
 
         return (
-            <div>
+            <div className="ingreTable">
                 <h3>Ingredients:</h3>
                 <DataTable
                     shadow={5}
