363aab50463a08196b6e9189208ab73cdee9a51c
Nathan Swanson <swanson4@uw.edu>
2016-12-06 19:06:05 -0800

    Add css to advanced search form

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router';
-import {Button, Textfield} from 'react-mdl';
+import { Link } from 'react-router';
+import { Button, Textfield } from 'react-mdl';
 
 class Search extends React.Component {
     constructor(props) {
@@ -37,130 +37,133 @@ class Search extends React.Component {
         return params;
     }
 
+
     render() {
         return (
-            <div>
+            <div className="adv-search">
                 <h1>Advanced Search</h1>
-                <div>
-                    <Textfield
-                        onChange={e => {this.setState({query:e.target.value})}}
-                        value={this.state.query}
-                        label="Recipe name"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => {this.setState({cuisine:e.target.value})}}
-                        value={this.state.cuisine}
-                        label="Cuisine type"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => {this.setState({includeIngredients:e.target.value})}}
-                        value={this.state.includeIngredients}
-                        label="Include ingredients"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => {this.setState({intolerances:e.target.value})}}
-                        value={this.state.intolerances}
-                        label="Allergies"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => {this.setState({diet:e.target.value})}}
-                        value={this.state.diet}
-                        label="Diet type (vegetarian, vegan, etc.)"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => {this.setState({excludeIngredients:e.target.value})}}
-                        value={this.state.excludeIngredients}
-                        label="Exclude ingredients"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                </div>
+                <div className="advanced-search">
+                    <div className="adv-search-fields" >
+                        <Textfield
+                            onChange={e => { this.setState({ query: e.target.value }) } }
+                            value={this.state.query}
+                            label="Recipe name"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => { this.setState({ cuisine: e.target.value }) } }
+                            value={this.state.cuisine}
+                            label="Cuisine type"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => { this.setState({ includeIngredients: e.target.value }) } }
+                            value={this.state.includeIngredients}
+                            label="Include ingredients"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => { this.setState({ intolerances: e.target.value }) } }
+                            value={this.state.intolerances}
+                            label="Allergies"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => { this.setState({ diet: e.target.value }) } }
+                            value={this.state.diet}
+                            label="Diet type (vegetarian, vegan, etc.)"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => { this.setState({ excludeIngredients: e.target.value }) } }
+                            value={this.state.excludeIngredients}
+                            label="Exclude ingredients"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                    </div>
 
-                <div>
-                    <Textfield
-                        onChange={e => this.setState({minCalories:e.target.value})}
-                        value={this.state.minCalories}
-                        pattern="-?[0-9]*(\.[0-9]+)?"
-                        error="Input is not a number!"
-                        label="Minimum calories"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => this.setState({maxCalories:e.target.value})}
-                        value={this.state.maxCalories}
-                        pattern="-?[0-9]*(\.[0-9]+)?"
-                        error="Input is not a number!"
-                        label="Maximum calories"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => this.setState({minCarbs:e.target.value})}
-                        value={this.state.minCarbs}
-                        pattern="-?[0-9]*(\.[0-9]+)?"
-                        error="Input is not a number!"
-                        label="Minimum carbs"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => this.setState({maxCarbs:e.target.value})}
-                        value={this.state.maxCarbs}
-                        pattern="-?[0-9]*(\.[0-9]+)?"
-                        error="Input is not a number!"
-                        label="Maximum carbs"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => this.setState({minFat:e.target.value})}
-                        value={this.state.minFat}
-                        pattern="-?[0-9]*(\.[0-9]+)?"
-                        error="Input is not a number!"
-                        label="Minimum fat"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => this.setState({maxFat:e.target.value})}
-                        value={this.state.maxFat}
-                        pattern="-?[0-9]*(\.[0-9]+)?"
-                        error="Input is not a number!"
-                        label="Maximum fat"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => this.setState({minProtein:e.target.value})}
-                        value={this.state.minProtein}
-                        pattern="-?[0-9]*(\.[0-9]+)?"
-                        error="Input is not a number!"
-                        label="Minimum protein"
-                        className="searchInput"
-                        floatingLabel
-                    />
-                    <Textfield
-                        onChange={e => this.setState({maxProtein:e.target.value})}
-                        value={this.state.maxProtein}
-                        pattern="-?[0-9]*(\.[0-9]+)?"
-                        error="Input is not a number!"
-                        label="Maximum protein"
-                        className="searchInput"
-                        floatingLabel
-                    />
+                    <div className="adv-search-fields">
+                        <Textfield
+                            onChange={e => this.setState({ minCalories: e.target.value })}
+                            value={this.state.minCalories}
+                            pattern="-?[0-9]*(\.[0-9]+)?"
+                            error="Input is not a number!"
+                            label="Minimum calories"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => this.setState({ maxCalories: e.target.value })}
+                            value={this.state.maxCalories}
+                            pattern="-?[0-9]*(\.[0-9]+)?"
+                            error="Input is not a number!"
+                            label="Maximum calories"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => this.setState({ minCarbs: e.target.value })}
+                            value={this.state.minCarbs}
+                            pattern="-?[0-9]*(\.[0-9]+)?"
+                            error="Input is not a number!"
+                            label="Minimum carbs"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => this.setState({ maxCarbs: e.target.value })}
+                            value={this.state.maxCarbs}
+                            pattern="-?[0-9]*(\.[0-9]+)?"
+                            error="Input is not a number!"
+                            label="Maximum carbs"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => this.setState({ minFat: e.target.value })}
+                            value={this.state.minFat}
+                            pattern="-?[0-9]*(\.[0-9]+)?"
+                            error="Input is not a number!"
+                            label="Minimum fat"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => this.setState({ maxFat: e.target.value })}
+                            value={this.state.maxFat}
+                            pattern="-?[0-9]*(\.[0-9]+)?"
+                            error="Input is not a number!"
+                            label="Maximum fat"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => this.setState({ minProtein: e.target.value })}
+                            value={this.state.minProtein}
+                            pattern="-?[0-9]*(\.[0-9]+)?"
+                            error="Input is not a number!"
+                            label="Minimum protein"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                        <Textfield
+                            onChange={e => this.setState({ maxProtein: e.target.value })}
+                            value={this.state.maxProtein}
+                            pattern="-?[0-9]*(\.[0-9]+)?"
+                            error="Input is not a number!"
+                            label="Maximum protein"
+                            className="searchInput"
+                            floatingLabel
+                            />
+                    </div>
                 </div>
-                <Link to={{pathname: '/search/results', query: this.trimParams()}}>
+                <Link to={{ pathname: '/search/results', query: this.trimParams() }}>
                     <Button raised colored>Search</Button>
                 </Link>
             </div>
