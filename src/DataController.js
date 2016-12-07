f5f65355c4a734eb3e1035972988a61f3e80d52b
Lucas Baker <luke.baker7@gmail.com>
2016-12-07 11:52:40 -0800

    Commented DataController

--- src/DataController.js
+++ src/DataController.js
@@ -3,6 +3,13 @@ import 'es6-promise';
 import * as firebase from 'firebase';
 
 class DataController {
+
+    /**
+     * Makes a request to the api
+     * @param endpoint The api endpoint to request from, e.g. "/search/results"
+     * @param parameters The parameters to pass to the request. Either in object form or a formatted string
+     * @param callback The function to call when the request is completed
+     */
     static makeRequest(endpoint, parameters, callback) {
         const baseUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com';
         const key = 'i7gEqPMlZGmshfpAA5Rtot8sVzWvp1BSR8ijsnBtIWIgxXFuDE';
@@ -19,7 +26,7 @@ class DataController {
             paramString = parameters;
         }
 
-
+        //Set headers, omit credentials to avoid CORS headaches
         let request = new Request(baseUrl + endpoint + paramString, {
             credentials: 'omit',
             headers: new Headers({
@@ -35,11 +42,14 @@ class DataController {
         })
     }
 
+    /**
+     * Signs the user in with the given email and password
+     */
     static signIn(email, password, callback) {
         firebase.auth().signInWithEmailAndPassword(email, password).then(user => {
             callback(user);
         }).catch(e => {
-            if (e.code === 'auth/user-not-found') {
+            if (e.code === 'auth/user-not-found') { //Create the account if it doesn't already exist
                 this.createUser(email, password, callback);
             }
             console.log(e.message)
@@ -47,10 +57,16 @@ class DataController {
         })
     }
 
+    /**
+     * Sign the user out
+     */
     static signOut() {
         firebase.auth().signOut();
     }
 
+    /**
+     * Create the user account
+     */
     static createUser(email, password, callback) {
         firebase.auth().createUserWithEmailAndPassword(email, password).then(user => {
             callback(user);
@@ -59,16 +75,25 @@ class DataController {
         });
     }
 
+    /**
+     * Set the user's display name
+     */
     static setName(name) {
         if (!this.getUser().displayName) {
             this.getUser().updateProfile({displayName: name, photoURL: null})
         }
     }
 
+    /**
+     * Returns the current user, null if no user is signed in
+     */
     static getUser() {
         return firebase.auth().currentUser;
     }
 
+    /**
+     * Submits the given comment for the given recipe id.
+     */
     static submitComment(id, comment) {
         let user = this.getUser();
 
@@ -79,10 +104,16 @@ class DataController {
         })
     }
 
+    /**
+     * Gets the comments for the given recipes
+     */
     static getComments(id, callback) {
         firebase.database().ref('recipes/' + id).on('value', snapshot => callback(snapshot));
     }
 
+    /**
+     * Register a listener for authentication changes
+     */
     static registerAuthListener(callback) {
         firebase.auth().onAuthStateChanged(callback);
     }
